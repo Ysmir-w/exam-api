7f99695ebca17047b096a8b9ad30532315a332d8
Ysmir <190126157@qq.com>
2023-01-01 15:15:52 +0800

    登录拦截器派出了登录接口

--- src/main/java/org/han/examination/config/ApplicationConfig.java
+++ src/main/java/org/han/examination/config/ApplicationConfig.java
@@ -13,6 +13,6 @@ public class ApplicationConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
     }
 }
